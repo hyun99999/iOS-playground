0dce4514568628498d85d347b6986d7e6b4144ce
hyungyu Kim <ypsophila97@icloud.com>
2021-01-03 01:06:13 +0900

    edit layout

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by kimhyungyu on 2021/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var slider :UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func sliderValueChanged(_ sender : UISlider) {
+        print(sender.value)
+    }
+    @IBAction func touchUpHitButton(_ sender : UIButton){
+        print(slider.value)
+    }
+}
+
