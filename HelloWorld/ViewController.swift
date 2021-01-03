06657464a25970f17679b1717ad0d3b7ae29ac56
hyungyu Kim <ypsophila97@icloud.com>
2021-01-04 01:20:45 +0900

    Use auto layout

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -15,11 +15,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
     @IBAction func sliderValueChanged(_ sender : UISlider) {
         print(sender.value)
     }
     @IBAction func touchUpHitButton(_ sender : UIButton){
         print(slider.value)
     }
+    @IBAction func touchUpResetButton(_ sender : UIButton){
+        print("touch up reset button")
+    }
+    @IBAction func 
 }
 
