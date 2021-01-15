8e279b5743dcda3312be8659a9c0232d3cb528da
hyungyu Kim <ypsophila97@icloud.com>
2021-01-15 13:37:36 +0900

    Initial Commit

---
+++ completionBlockTutorial-iOS-practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  completionBlockTutorial-iOS-practice
+//
+//  Created by kimhyungyu on 2021/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
