ca4972ae24c56ff0ea9622552103f2de902d6c37
hyungyu Kim <ypsophila97@icloud.com>
2021-01-15 17:28:45 +0900

    Use progress bar to represent completion block

--- completionBlockTutorial-iOS-practice/ViewController.swift
+++ completionBlockTutorial-iOS-practice/ViewController.swift
@@ -6,15 +6,24 @@
 //
 
 import UIKit
+import KRProgressHUD
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("ViewController - viewDidLoad() called")
+        //progressbar
+        KRProgressHUD.show()
         sayHi(completion: {result in
             print("\(result)")
+            KRProgressHUD.showSuccess()
+            //progress bar 가 성공하면 라벨의 내용을 바꾼다.
+            self.mainTitle.text = result
+            
         })
     }
 
@@ -24,7 +33,7 @@ class ViewController: UIViewController {
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
 //            print("hi-delay")
             //sayhi가 끝나는 시점을 전달
-            completion("hi")
+            completion("hi!")
         }
     }
 }
