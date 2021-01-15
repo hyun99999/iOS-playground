6125365eb6a5676957a0e57f7c2e1c4839dad596
hyungyu Kim <ypsophila97@icloud.com>
2021-01-15 17:01:59 +0900

    Install progress bar open source

--- completionBlockTutorial-iOS-practice/ViewController.swift
+++ completionBlockTutorial-iOS-practice/ViewController.swift
@@ -12,8 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("ViewController - viewDidLoad() called")
+        sayHi(completion: {result in
+            print("\(result)")
+        })
     }
 
-
+    fileprivate func sayHi(completion: @escaping(String) -> ()){
+        print("hi")
+        //딜레이 주기.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+//            print("hi-delay")
+            //sayhi가 끝나는 시점을 전달
+            completion("hi")
+        }
+    }
 }
 
